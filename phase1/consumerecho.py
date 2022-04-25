d2ecfafcc1b4bc9565bfc22157b758475034521a
Jason <leejason1990@gmail.com>
2022-04-25 10:08:16 -0400

    started

--- phase1/consumerecho.py
+++ phase1/consumerecho.py
@@ -1,5 +1,6 @@
 from kafka import KafkaConsumer
 from json import loads
+import sqlalchemy
 
 consumer = KafkaConsumer(
     'bank-customer-events',
@@ -10,3 +11,13 @@ for message in consumer:
     print(message)
     message = message.value
     print('{} found'.format(message))
+
+class Transaction(Base):
+    __tablename__ = 'transaction'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    custid = Column(Integer)
+    type = Column(String(250), nullable=False)
+    date = Column(Integer)
+    amt = Column(Integer)
